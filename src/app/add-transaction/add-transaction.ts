0d78d2ff1e8030f16825e694d41ed3e7430649d3
abhishek <abhishekhegde122002@gmail.com>
2026-01-20 18:55:44 +0530

    add-transaction2

--- src/app/add-transaction/add-transaction.ts
+++ src/app/add-transaction/add-transaction.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ReactiveFormsModule , FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-transaction',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './add-transaction.html',
   styleUrl: './add-transaction.css',
 })
 export class AddTransactionComponent {
+  txForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.txForm = this.fb.group({
+      date: ['', Validators.required],
+      amount: ['', Validators.required],
+      type: ['', Validators.required],
+      party: ['', Validators.required],
+      txnNo: ['', Validators.required],
+      vendor: ['', Validators.required],
+      remarks: ['']
+    });
+  }
+
+  save() {
+    if (this.txForm.invalid) return;
+
+    console.log('Transaction saved:', this.txForm.value);
+
+    //API Calling
+
+    this.router.navigate(['/view-transaction']);
+  }
 }
